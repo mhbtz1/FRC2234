54e1470173d6d87cd8971095053259a5d5944bf4
Matthew Habtezgi <mtarekegn143@gmail.com>
2021-02-16 17:16:01 -0500

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,5 +1,6 @@
 package frc.robot;
 import java.util.*;
+import java.io.*;
 import edu.wpi.first.wpilibj.*;
 
 
@@ -13,12 +14,20 @@ public class Profile {
     public Location MY_CURRENT_POSITION;
     public static double DISTANCE_UPPER_THRESHOLD = 30;
     public static double DISTANCE_LOWER_THRESHOLD = 0;
+    public ArrayList<Location> true_waypoints;
+    public ArrayList<Location> waypoints;
+    public ArrayList<BezierProfile> bez;
+    public boolean ANGLE_FILE_HAS_BEEN_INITIALIZED;
     public Profile(ArrayList<Location> path){
         this.path = path;
         velocity_profile = new HashMap<Integer, Location>();
         acceleration_profile = new HashMap<Integer, Location>();
         angle_profile = new HashMap<Integer, Double>();
         MY_CURRENT_POSITION = new Location(0,0);
+        this.true_waypoints = new ArrayList<Location>();
+        this.waypoints = new ArrayList<Location>();
+        this.bez = new ArrayList<BezierProfile>();
+        ANGLE_FILE_HAS_BEEN_INITIALIZED = false;
     }
     public void resetLocation(){
         MY_CURRENT_POSITION = new Location(0,0);
@@ -39,6 +48,65 @@ public class Profile {
             cur_pos= path.get(i);
         }
     }
+    public boolean loc_contains(ArrayList<Location> targ, Location tst){
+        for(Location l : targ){
+          if(l.equals(tst)){
+            return true;
+          }
+        }
+        return false;
+      }
+    
+
+    public ArrayList<Location> parseFile(){
+        ArrayList<Integer> maintain_hashcodes = new ArrayList<Integer>();
+        ArrayList<Location> ans = new ArrayList<Location>();
+        //String[] r = loadStrings("controlPoints.txt");
+        ArrayList<Location> remove_duplicates = new ArrayList<Location>();
+        try{
+            String s = "C:\\Users\\Core\\pathfinding_stuff-Imported\\src\\main\\java\\frc\\robot\\controlPathBehavior.txt";
+            BufferedReader r = new BufferedReader(new FileReader(s) );
+            String inp = "";
+            while( (inp = r.readLine() ) != null){
+            double one = Double.parseDouble(inp.substring(0, inp.indexOf(":")));
+            double two = Double.parseDouble(inp.substring(inp.indexOf(":")+1));
+            int hc = (new Location( (float)(one), (float)(two) ) ).hashCode();
+            if(loc_contains(remove_duplicates, new Location( (float)(one), (float)(two) ) ) ){continue;}
+            remove_duplicates.add(new Location( (float)(one), (float)(two) ) );
+            ans.add(new Location( (float)(one), (float)(two) ) );
+            //println("POINT: " + one + " " + two);
+            }
+            r.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return new ArrayList<Location>(remove_duplicates);
+    }
+
+    public void iterate_profiles(){
+        if(waypoints.size() == 0){
+          waypoints = parseFile();
+          int ITER = 200;
+          if(bez.size() == 0){
+            for(int i = 0; i < waypoints.size()-ITER; i+= ITER){
+              //println(waypoints.get(i).x + " " + waypoints.get(i).y);
+              BezierProfile b = new BezierProfile(waypoints.get(i).x,waypoints.get(i).y,waypoints.get(i+ITER/4).x,waypoints.get(i+ITER/4).y,waypoints.get(i+ITER/2).x,waypoints.get(i+ITER/2).y,waypoints.get(i+ITER).x,waypoints.get(i+ITER).y);
+              bez.add(b);
+            }
+          }
+        }
+        for(BezierProfile b : bez){
+          for(float j = 0; j <= 1.0; j += 0.04){
+            //println(b.return_x(j) + " " + b.return_y(j));
+            true_waypoints.add(new Location(b.return_x(j), b.return_y(j)));
+          }
+        }
+        if(!ANGLE_FILE_HAS_BEEN_INITIALIZED){
+            constructAngleMap();
+            ANGLE_FILE_HAS_BEEN_INITIALIZED=true;
+        }
+      }
+
     public void constructAccelerationMap(){
         if(path == null){
             return;
@@ -48,16 +116,29 @@ public class Profile {
             acceleration_profile.put(i, acc);
         }
     }
-    public void constructAngleMap(){
-        if(path == null){
-            return;
+    public HashMap<Integer,Double> constructAngleMap(){
+        HashMap<Integer,Double> angle_profile = new HashMap<Integer,Double>();
+        if(true_waypoints == null){
+            return new HashMap<Integer,Double>();
         }
-        for(int i = 0; i < path.size() - 1; i++){
-            double avl = Math.atan( (double)( path.get(i+1).x - path.get(i).x)/(double)(path.get(i+1).y - path.get(i).y) ) * ((double)(180)/(double)(Math.PI));
-            angle_profile.put(i,avl);
-            System.out.println("WAYPOINT INDEX: " + i + "ANGLE: " + avl);
+        try{ 
+            String s = "C:\\Users\\Core\\pathfinding_stuff-Imported\\src\\main\\java\\frc\\robot\\the_angles.txt";
+            BufferedWriter  r= new BufferedWriter( new FileWriter( s ) );
+            for(int i = 0; i < true_waypoints.size() - 1; i++){
+                double avl = Math.atan( (double)( true_waypoints.get(i+1).y - true_waypoints.get(i).y)/(double)(true_waypoints.get(i+1).x - true_waypoints.get(i).x) ) * ((double)(180)/(double)(Math.PI));
+                angle_profile.put(i,avl);
+                System.out.println("ANGLE AT TIME: " + i +  " IS: "  + avl);
+                r.write(Double.toString(avl).toCharArray());
+                System.out.println("WAYPOINT INDEX: " + i + "ANGLE: " + avl);
+            }
+            r.close();
+        } catch(Exception e){
+            e.printStackTrace();
         }
+        System.out.println("ANGLES HAVE BEEN ASSIGNED");
+        return angle_profile;
     }
+
     public void angle_dead_reckoning(){
         angle_profile.put(0,-90.0);
         angle_profile.put(1,-180.0);
@@ -81,3 +162,4 @@ public class Profile {
     }
     
 }
+
