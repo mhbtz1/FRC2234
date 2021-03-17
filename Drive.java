afff20351497dd882d97a772f6b35da35f908102
Matthew Habtezgi <mtarekegn143@gmail.com>
2021-03-17 17:17:18 -0400

    Update Drive.java

--- Drive.java
+++ Drive.java
@@ -17,6 +17,7 @@ public class Drive{
     boolean ANGLE_HANDLER = false;
     int angle_ptr;
     int obstacle_ptr;
+    int bezier_ptr;
     double cur_angle= 0;
     boolean IS_MODULATING_ANGLE = false;
     public Drive(Joystick j1, CANSparkMax c1, CANSparkMax c2, ADXRS450_Gyro g1){
@@ -30,6 +31,7 @@ public class Drive{
         angle_ptr = 0;
         INTERNAL_COUNTER = 0;
         obstacle_ptr = 0;
+        bezier_ptr = 0;
     }
     public Drive(CANSparkMax c1, CANSparkMax c2, ADXRS450_Gyro g1){
         this.c1 = c1;
@@ -70,7 +72,62 @@ public class Drive{
     
 
 
+    //implements the autonav with the bezier curves for motion profiling
+    public void modifiedDriveTrapezoidal(Profile p){
+        if(waypoints == null){
+            return;
+        }   
+        //THIS MOVES FORWARD
+        //pc1.setReference(1.5, ControlType.kVoltage);
+        //pc2.setReference(-1.5, ControlType.kVoltage);
+        //THIS MOVES BACKWARD
+        //pc1.setReference(-1.5, ControlType.kVoltage);
+        //pc2.setReference(1.5, ControlType.kVoltage);
+
+        try {
+            if(obstacle_ptr < p.true_waypoints.size()){
+                if(!IS_MODULATING_ANGLE){
+                    System.out.println("OBSTACLE POINTER: " + obstacle_ptr);
+                    //the bezier profile represents an ordered pair of time and velocity
+                    ArrayList<Location> bezvel = p.bezier_velocity.get(obstacle_ptr).parametrized_speeds;
+                    double mag1 = bezvel.get(bezier_ptr).y;
+                    System.out.println("MAGNTIUDE AT THE MOMENT: " + mag1);
+                    System.out.println("CURRENT TARGET ANGLE: " + p.angle_profile.get(obstacle_ptr));
+                    if(!p.isWithinThreshold(p.true_waypoints.get(obstacle_ptr))){
+                        ++bezier_ptr;
+                        pc1.setReference(mag1/(double)(4), ControlType.kVoltage);
+                        pc2.setReference(-mag1/(double)(4), ControlType.kVoltage);
+                        p.updateMyPosition(p.velocity_profile.get(obstacle_ptr),cur_angle);
+                        System.out.println("X POSITION: " + p.MY_CURRENT_POSITION.x);
+                        System.out.println("Y POSITION: " + p.MY_CURRENT_POSITION.y);
+                        System.out.println("TARGET POSITION: " + p.true_waypoints.get(obstacle_ptr).x);
+                        System.out.println("TARGET POSITION: " + p.true_waypoints.get(obstacle_ptr).y);
+                    } else {
+                        System.out.println("MODULATING ANGLE ON");
+                        IS_MODULATING_ANGLE=true;
+                        bezier_ptr = 0;
+                    }
+                } else {
+                    //System.out.println("THRESHOLD HAS BEEN REACHED, MODULATING ANGLE.");
+                    if(angleUpdate(p.angle_profile.get(obstacle_ptr), new PIDControl(Robot.P, Robot.I, Robot.D))){
+                        cur_angle = p.angle_profile.get(obstacle_ptr);
+                        ++obstacle_ptr;
+                        bezier_ptr = 0;
+                        System.out.println("ANGLE MODULATION COMPLETE!");
+                        IS_MODULATING_ANGLE=false;
+                    }
+                }
+            }
+        } catch(NullPointerException e){
+            e.printStackTrace();
+        }
+
+    }
+
     
+    //implements the autonav without the bezier curves for motion profiling
+    //(it basically just discerns where it is and where it should be next and 
+    //sets its velocity to be the distance between them scaled by some factor)
     public void modifiedDrive(Profile p){
         if(waypoints == null){
             return;
@@ -89,8 +146,8 @@ public class Drive{
                     System.out.println("MAGNTIUDE AT THE MOMENT: " + mag1);
                     System.out.println("CURRENT TARGET ANGLE: " + p.angle_profile.get(obstacle_ptr));
                     if(!p.isWithinThreshold(p.true_waypoints.get(obstacle_ptr))){
-                        pc1.setReference(mag1/5, ControlType.kVoltage);
-                        pc2.setReference(-mag1/5, ControlType.kVoltage);
+                        pc1.setReference(mag1/(double)(4), ControlType.kVoltage);
+                        pc2.setReference(-mag1/(double)(4), ControlType.kVoltage);
                         p.updateMyPosition(p.velocity_profile.get(obstacle_ptr),cur_angle);
                         System.out.println("X POSITION: " + p.MY_CURRENT_POSITION.x);
                         System.out.println("Y POSITION: " + p.MY_CURRENT_POSITION.y);
