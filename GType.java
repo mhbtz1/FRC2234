2e3f678d444e33941268938d45437b38ff21196c
Matthew Habtezgi <mtarekegn143@gmail.com>
2021-04-30 15:09:47 -0400

    Update GType.java

--- GType.java
+++ GType.java
@@ -4,11 +4,11 @@ class GType{
   float HEURISTIC_COST;
   public GType(PVector evec, float COST_TO_REACH){
     this.evec = evec;
-    this.COST_TO_REACH=COST_TO_REACH;
-  }
-  public GType(PVector evec,float COST_TO_REACH, float HEURISTIC_COST){
-    this.evec= evec;
-    this.HEURISTIC_COST = HEURISTIC_COST;
     this.COST_TO_REACH = COST_TO_REACH;
+  }
+  public GType(PVector evec, float COST_TO_REACH, float HEURISTIC_DIST){
+    this.evec = evec;
+    this.COST_TO_REACH = COST_TO_REACH;
+    this.HEURISTIC_COST = HEURISTIC_COST;
   }
 }
